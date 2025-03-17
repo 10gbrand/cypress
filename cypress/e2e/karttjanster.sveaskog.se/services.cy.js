ff22716f0c331031d8ded22207fe3927f35c0cfb
mate01 <mattias.tengbrand@sveaskog.se>
2025-03-17 14:50:32 +0100

    funkar mot karttjanster

--- cypress/e2e/karttjanster.sveaskog.se/services.cy.js
+++ cypress/e2e/karttjanster.sveaskog.se/services.cy.js
@@ -1,18 +1,39 @@
 describe("Test Services page on AGS", () => {
     it("Services page", () => {
-        cy.fixture('karttjanster.services'.then((data) => {
-          const  restServices=data 
-        }))
-        cy.request(
-            {
+        cy.fixture('karttjansterServices').then((restServices) => {
+            cy.log('Fixture data:', JSON.stringify(restServices));
+
+            cy.request({
                 method: 'GET',
-                url: `${Cypress.env('karttjanster')}/services?f=pjson`
-            }
-        )
-            .then((response) => {
-                expect(response.status).to.eq(200)
-                expect(response.body.currentVersion).to.eq(10.91)
-        })
-    })
-})
+                url: `${Cypress.env('karttjanster')}/services?f=pjson`,
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                  }
+            })
+                .then((response) => {
+                    const parsedBody = JSON.parse(response.body);
+                    
+                    // Log the entire response
+                    //console.log('Full response:', response);
+                    //cy.log('Full response:', JSON.stringify(response));
+
+                    // Log specific parts
+                    //console.log('Response body:', parsedBody);
+                    //console.log('Current Version:', parsedBody.currentVersion);
+                    
+                    // Assertions
+                    expect(response.status).to.eq(200);
+                    expect(parsedBody.currentVersion).to.eq(10.91);
+                    expect(parsedBody.currentVersion).to.eq(restServices.currentVersion);
+                    expect(parsedBody).to.eq(restServices);
+
+                    // Additional checks
+                    expect(parsedBody).to.have.property('folders').that.is.an('array');
+                    expect(parsedBody).to.have.property('services').that.is.an('array');
+            });
+        });
+    });
+});
+
 

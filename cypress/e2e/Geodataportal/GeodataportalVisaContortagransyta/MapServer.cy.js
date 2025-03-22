0a531dd2448bf1bf273af9bd1c68c604bf44fbf5
Mattias Tengbrand <mattias.tengbrand@sveaskog.se>
2025-03-22 16:32:28 +0100

    testerna kör men ger fel

---
+++ cypress/e2e/Geodataportal/GeodataportalVisaContortagransyta/MapServer.cy.js
@@ -0,0 +1,36 @@
+const RERUNS = 3;
+const URLSUFIX = Cypress.env('karttjansterSites');
+
+describe("Geodataportal/GeodataportalVisaContortagransyta", () => {
+    URLSUFIX.forEach((urlsf) => {
+        const KarttjansURL = `${Cypress.env('karttjansterBase')}${urlsf}.${Cypress.env('sveaDomain')}/${Cypress.env('karttjansterRest')}`;
+        Cypress._.times(RERUNS, (index) => {
+            it(`MapServer - ${KarttjansURL}/services/Geodataportal/GeodataportalVisaContortagransyta/MapServer?f=pjson - Run ${index + 1}`, () => {
+                cy.fixture('karttjanster/Geodataportal/GeodataportalVisaContortagransyta/MapServer').then((fixture) => {
+                    cy.log('Fixture data:', JSON.stringify(fixture));
+                    cy.request({
+                        method: 'GET',
+                        url: `${KarttjansURL}/services/Geodataportal/GeodataportalVisaContortagransyta/MapServer?f=pjson`,
+                        headers: {
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json'
+                        }
+                    })
+                        .then((response) => {
+                            const parsedBody = JSON.parse(response.body);
+                        
+                            // Log specific parts
+                            cy.log('Response body:', parsedBody);
+
+                            // Assertions
+                            expect(response.status).to.eq(200);
+                            expect(parsedBody.currentVersion).to.eq(fixture.currentVersion);
+                            expect(parsedBody.layers).to.deep.equal(fixture.layers);
+                            expect(parsedBody.spatialReference).to.deep.equal(fixture.spatialReference);
+                            expect(parsedBody.tables).to.deep.equal(fixture.tables);
+                        });
+                });
+            });
+        });
+    });
+});
---
+++ cypress/e2e/Geodataportal/GeodataportalVisaNaturkultur/MapServer.cy.js
@@ -0,0 +1,36 @@
+const RERUNS = 3;
+const URLSUFIX = Cypress.env('karttjansterSites');
+
+describe("Geodataportal/GeodataportalVisaNaturkultur", () => {
+    URLSUFIX.forEach((urlsf) => {
+        const KarttjansURL = `${Cypress.env('karttjansterBase')}${urlsf}.${Cypress.env('sveaDomain')}/${Cypress.env('karttjansterRest')}`;
+        Cypress._.times(RERUNS, (index) => {
+            it(`MapServer - ${KarttjansURL}/services/Geodataportal/GeodataportalVisaNaturkultur/MapServer?f=pjson - Run ${index + 1}`, () => {
+                cy.fixture('karttjanster/Geodataportal/GeodataportalVisaNaturkultur/MapServer').then((fixture) => {
+                    cy.log('Fixture data:', JSON.stringify(fixture));
+                    cy.request({
+                        method: 'GET',
+                        url: `${KarttjansURL}/services/Geodataportal/GeodataportalVisaNaturkultur/MapServer?f=pjson`,
+                        headers: {
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json'
+                        }
+                    })
+                        .then((response) => {
+                            const parsedBody = JSON.parse(response.body);
+                        
+                            // Log specific parts
+                            cy.log('Response body:', parsedBody);
+
+                            // Assertions
+                            expect(response.status).to.eq(200);
+                            expect(parsedBody.currentVersion).to.eq(fixture.currentVersion);
+                            expect(parsedBody.layers).to.deep.equal(fixture.layers);
+                            expect(parsedBody.spatialReference).to.deep.equal(fixture.spatialReference);
+                            expect(parsedBody.tables).to.deep.equal(fixture.tables);
+                        });
+                });
+            });
+        });
+    });
+});
